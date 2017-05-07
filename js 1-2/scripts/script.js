4fe42382a5153ea6d38e4ac0ee2152a622bfbff3
Artem <skripka.artem@i.ua>
2017-05-07 17:58:37 +0300

    1-2

--- js 1-2/scripts/script.js
+++ js 1-2/scripts/script.js
@@ -1,29 +1,32 @@
-// Home1
-// 'use string';
- // function pow(a, n) {
- //     var result = a;
- //
- //     for (var i = 0; i < n; i++) {
- //        result = result*a;
- //    }
- //
- //    return result;
- // }
- //
- // var a = prompt("a?", '');
- // var n = prompt("n?", '');
- // console.log( pow(a, n) );
+﻿// Home1
+ 'use string';
+alert('HW1')
+console.log( 'HW1' );
+ function pow(a, n) {
+      var result = a;
 
+    for (var i = 0; i < n; i++) {
+       result = result*a;
+   }
+
+    return result;
+ }
+
+ var a = prompt("Введите число a?", '');
+ var n = prompt("Введите степень n?", '');
+ console.log( pow(a, n) );
+alert('Решение смотреть в консоле')
 
 //Home2
+alert('HW2')
 var arr = [];
  for (var i=0; i<5; i++) {
      arr[i] = prompt('Введите имя');
  }
 
- console.log(arr);
-if ( arr.indexOf( prompt('Введите имя пользователя') ) != -1) {
-    alert('вы успешно вошли')
+ var avtor = prompt('Введите имя пользователя');
+if ( arr.indexOf(avtor ) != -1) {
+    alert(avtor +'вы успешно вошли')
 } else {
     alert('Ошибка, пользователя не существует')
 }
