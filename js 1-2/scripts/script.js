5fa8557281dc705177429bab3739646af1624a92
Artem <skripka.artem@i.ua>
2017-05-10 20:20:37 +0300

    1-2

--- js 1-2/scripts/script.js
+++ js 1-2/scripts/script.js
@@ -3,40 +3,38 @@
 alert('HW1')
 console.log( 'HW1' );
 
- for (i=0; i=isNaN(a); i++) {
-     var a = prompt("Введите число a?", '');
- }
-for (i=0; i=isNaN(n); i++) {
-    var n = prompt("Введите число n?", '');
-}
+
 
  function pow(a, n) {
       var result = a;
 
-    for (var i = 0; i < n; i++) {
+
+     if(isNaN(a) || isNaN(n)) {
        result = result*a;
    }
 
     return result;
  }
+var a = prompt("Введите число a?", '');
 
+var n = prompt("Введите число n?", '');
 
 
  console.log( pow(a, n) );
-alert('Решение смотреть в консоле')
+ alert('Решение смотреть в консоле')
 
-//Home2
+ //Home2
 alert('HW2')
-var arr = [];
- for (var i=0; i<5; i++) {
-     arr[i] = prompt('Введите имя');
+ var arr = [];
+  for (var i=0; i<5; i++) {
+      arr[i] = prompt('Введите имя');
  }
 
- var avtor = prompt('Введите имя пользователя');
-if ( arr.indexOf(avtor ) != -1) {
+  var avtor = prompt('Введите имя пользователя');
+ if ( arr.indexOf(avtor ) != -1) {
     alert(avtor +'вы успешно вошли')
-} else {
-    alert('Ошибка, пользователя не существует')
-}
+ } else {
+     alert('Ошибка, пользователя не существует')
+ }
 
 
