7c05dd9ccbddf1d6f89a2a83ff899fc8f6cf3f87
Artem <skripka.artem@i.ua>
2017-06-28 13:41:01 +0300

    15-16

---
+++ js15-16/js/script.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  var container = document.getElementById('root'),
+    source = document.getElementById('article').textContent,
+    compiled = _.template(source),
+    apiUrl = 'https://pixabay.com/api/?key=5748773-95b30dc3ce1789fbe99be3f8e&per_page=10';
+
+  var promise = fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('ERROR while fetching!');
+  }).then(function(data) {
+    console.log(data);
+    var articles = _.map(data.hits, function(item) {
+      return {
+        img: item.previewURL
+
+      };
+    });
+
+    console.log(articles);
+
+    render(compiled, articles, container);
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+  function render(template, data, parent) {
+    var htmlString = '';
+
+    _.forEach(data, function(item) {
+      htmlString += template(item);
+    });
+
+    parent.innerHTML += htmlString;
+  }
+})();
+
+
+var API_KEY = '5748773-95b30dc3ce1789fbe99be3f8e';
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('car');
+
+$.getJSON(URL, function(data){
+    if (parseInt(data.totalHits) > 0)
+        $.each(data.hits, function(i, hit){ console.log(hit.previewURL); });
+    else
+        console.log('No hits');
+});
