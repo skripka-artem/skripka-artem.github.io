c690b8f6240cca7491afe5dbaef111e2f5e492fc
Artem <skripka.artem@i.ua>
2017-06-28 15:04:08 +0300

    15-16

--- js15-16/js/script.js
+++ js15-16/js/script.js
@@ -1,41 +1,37 @@
-(function() {
-  'use strict';
+$(document).ready(function() {
+'use strict';
 
-  var container = document.getElementById('root'),
-    source = document.getElementById('article').textContent,
-    compiled = _.template(source),
-    apiUrl = 'https://pixabay.com/api/?key=5748773-95b30dc3ce1789fbe99be3f8e&per_page=10';
+var parent = document.getElementById('container');
+var sourse = document.getElementById('template').textContent;
+var API_KEY = '5723966-7e68f329c0132750f857a39d3';
 
-  var promise = fetch(apiUrl).then(function(response) {
-    if (response.ok) {
+function getPictures() {
+
+var searchValue = document.getElementsByClassName('input')[0].value;
+console.log('searchValue = ', searchValue);
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(searchValue)+"&per_page=40";
+
+fetch(URL)
+  .then(function(response){
+    if (response.ok){
       return response.json();
     }
-    throw new Error('ERROR while fetching!');
-  }).then(function(data) {
+    throw new Error('ERROR fetching data!');
+  })
+  .then(function(data){
     console.log(data);
-    var articles = _.map(data.hits, function(item) {
-      return {
-        img: item.previewURL
-
-      };
-    });
-
-    console.log(articles);
-
-    render(compiled, articles, container);
-  }).catch(function(error) {
-    console.log(error);
+    var templateAPI = _.template(sourse);
+    parent.innerHTML = templateAPI(data);
+  })
+  .catch(function(error){
+    console.log('Error --- ', error);
   });
 
-  function render(template, data, parent) {
-    var htmlString = '';
+}
 
-    _.forEach(data, function(item) {
-      htmlString += template(item);
-    });
-
-    parent.innerHTML += htmlString;
+document.getElementsByClassName('button')[0].addEventListener("click", getPictures);
+document.getElementsByClassName('input')[0].addEventListener("keypress", function(e){
+  if(e.keyCode === 13){getPictures();}
   }
-})();
-
-
+    );
+});
