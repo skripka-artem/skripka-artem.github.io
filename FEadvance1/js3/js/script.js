ad6352383f1d04462475faa229267a5af784280c
Artem <skripka.artem@i.ua>
2018-01-28 14:14:55 +0200

    js3

---
+++ FEadvance1/js3/js/script.js
@@ -0,0 +1,11 @@
+const alphait = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+
+const keyboard =  alphait.split('') ;
+
+
+const hello = keyboard[17] + keyboard[2] + keyboard[20] + keyboard[20] + keyboard[8];
+const javascript = keyboard[18] + keyboard[12] + keyboard[26] + keyboard[12] + keyboard[13]  + keyboard[25]  + keyboard[3] + keyboard[7]  + keyboard[9]  + keyboard[4];
+const trainer = keyboard[4] + keyboard[3] + keyboard[12] + keyboard[7] + keyboard[28]  + keyboard[2] + keyboard[3] ;
+console.log(hello);
+console.log(javascript);
+console.log(trainer);
