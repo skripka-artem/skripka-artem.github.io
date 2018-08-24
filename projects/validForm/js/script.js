01fe8b2fa79ee447ab6dd280d94edae55a9ed94b
Artem <skripka.artem@i.ua>
2018-08-24 10:01:33 +0300

    1

--- projects/validForm/js/script.js
+++ projects/validForm/js/script.js
@@ -2,9 +2,12 @@ let firstNameBox = document.getElementById("firstName");
 let lastNameBox = document.getElementById("lastName");
 let emailBox = document.getElementById("email");
 let submit = document.getElementsByTagName("button")[0];
+let firstNameError = document.getElementById("firstNameError");
+let lastNameError = document.getElementById("lastNameError");
+let emailError = document.getElementById("emailError");
 
 let firstNameValid = function() {
-  if (firstNameBox.value.match(/^[A-zА-яЁё]+$/) == null) {
+  if (firstNameBox.value.match(/^[A-zА-яЁё]+$/) === null) {
     firstName = true;
     firstNameError.style.display = "block";
   } else {
@@ -13,8 +16,29 @@ let firstNameValid = function() {
   }
 };
 
+let firstNameValidText = function() {
+  if (firstNameBox.value=="") {
+    firstName = true;
+    firstNameError.style.display = "block";
+  } else {
+    firstName = false;
+    firstNameError.style.display = "none";
+  }
+};
+
+
 let lastNameValid = function() {
-  if (lastNameBox.value.match(/^[A-zА-яЁё]+$/) == null) {
+  if (lastNameBox.value.match(/^[A-zА-яЁё]+$/) === null) {
+    lastName = true;
+    lastNameError.style.display = "block";
+  } else {
+    lastName = false;
+    lastNameError.style.display = "none";
+  }
+};
+
+let lastNameValidText = function() {
+  if (lastNameBox.value=="") {
     lastName = true;
     lastNameError.style.display = "block";
   } else {
@@ -25,7 +49,7 @@ let lastNameValid = function() {
 
 let emailValid = function() {
   if (
-    emailBox.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/) ==
+    emailBox.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/) ===
     null
   ) {
     email = true;
@@ -45,6 +69,9 @@ let send = function() {
 };
 
 firstNameBox.addEventListener("keypress" , firstNameValid);
+firstNameBox.addEventListener("blur" , firstNameValidText);
 lastNameBox.addEventListener("keypress", lastNameValid);
+lastNameBox.addEventListener("blur", lastNameValidText);
 emailBox.addEventListener("blur", emailValid);
 submit.addEventListener("click", send);
+
