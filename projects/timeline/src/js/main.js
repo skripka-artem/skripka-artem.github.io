4e65e9be64630e95e067669c0b946fbe1dde6bc8
Артем Скрипка <artemskripka@MacBook-Air.local>
2020-03-12 22:10:07 +0200

    rezume

---
+++ projects/timeline/src/js/main.js
@@ -0,0 +1,14 @@
+
+    $('.your-class').slick({
+        slidesToShow: 1,
+        asNavFor: '.your-class2'
+});
+
+    $('.your-class2').slick({
+        centerMode: true,
+        focusOnSelect: true,
+        slidesToShow: 6,
+        infinite: false,
+        vertical: true,
+        asNavFor: '.your-class'
+    });
